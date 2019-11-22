7473e2ae64fec39efe57854965cab2d61a8e1319
Steve Vance <steve.vance@hotmail.co.uk>
2019-11-22 12:40:26 +0000

    sixth function stage

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,5 +8,12 @@ end
 
 def add_or_remove_cash(shop, amount)
   return shop[:admin][:total_cash] += amount
-
 end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, amount)
+  return shop[:admin][:pets_sold] += amount
+  end
